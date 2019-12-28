e6d0296a96a7f13e8e8f28615edd76a4ad750e06
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2019-12-28 14:12:13 +0545

    Retrofit base url created

---
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/Url/Url.java
@@ -0,0 +1,22 @@
+package com.example.traffic_rule_and_sign_quiz_app.Url;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Url{
+
+    private static final String BASE_URL="http://10.0.2.2:5000/";
+    //private static final String IMG_URL="http://10.0.2.2.4000/images/";
+
+    public static String token="Bearer";
+
+
+    public static Retrofit getInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Url.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit;
+    }
+}
