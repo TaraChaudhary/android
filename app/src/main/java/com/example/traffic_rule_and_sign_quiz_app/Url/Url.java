df696b2eba8bb15ba0f4ab39a11026bdcfb80f76
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2019-12-29 14:21:48 +0545

    Base url created

--- app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/Url/Url.java
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/Url/Url.java
@@ -5,12 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Url{
 
-    private static final String BASE_URL="http://10.0.2.2:5000/";
-    //private static final String IMG_URL="http://10.0.2.2.4000/images/";
-
+    private static final String BASE_URL="http://10.0.2.2:4000/";
     public static String token="Bearer";
-
-
     public static Retrofit getInstance() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Url.BASE_URL)
