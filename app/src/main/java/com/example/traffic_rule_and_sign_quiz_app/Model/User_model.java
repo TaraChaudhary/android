36bca3f805d650ee800cfd694a9ae5608f8245de
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2019-12-30 14:34:13 +0545

    Model user_model is created

---
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/Model/User_model.java
@@ -0,0 +1,107 @@
+package com.example.traffic_rule_and_sign_quiz_app.Model;
+
+public class User_model {
+
+    String _id;
+    String firstname;
+    String lastname;
+    String phone;
+    String gender;
+    String dob;
+    String email;
+    String username;
+    String password;
+    String token;
+
+    public User_model(String firstname, String lastname, String phone, String gender, String dob, String email, String username, String password, String token) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.gender = gender;
+        this.dob = dob;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.token = token;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
