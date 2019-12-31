baddd9213d65dab06f1d44cc9aee3fcaee74bb73
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2019-12-31 14:57:49 +0545

    Register with api

--- app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/API/User.java
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/API/User.java
@@ -1,4 +1,13 @@
 package com.example.traffic_rule_and_sign_quiz_app.API;
 
+import com.example.traffic_rule_and_sign_quiz_app.Model.User_model;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface User {
+    @POST("signup")
+    Call<Void> registerUser(@Body User_model user);
+
 }
