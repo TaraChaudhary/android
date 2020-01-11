39371edbfbf7895e6e28bef1ad8b8ba160599266
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2020-01-11 23:00:09 +0545

    Recycle dashboard design

--- app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/DashboardActivity.java
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/DashboardActivity.java
@@ -1,16 +1,44 @@
 package com.example.traffic_rule_and_sign_quiz_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.traffic_rule_and_sign_quiz_app.Myadapter.MyAdapter;
 import com.example.traffic_rule_and_sign_quiz_app.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        BottomNavigationView home_navigation;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-    }
-}
+
+        home_navigation = findViewById(R.id.homeNavigation);
+        ViewDashboardActivity dashboardFragment = new ViewDashboardActivity();
+        setFragment(dashboardFragment);
+        }
+
+@Override
+public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+        }
+
+public void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dasboard, fragment);
+        fragmentTransaction.commit();
+        }
+        }
