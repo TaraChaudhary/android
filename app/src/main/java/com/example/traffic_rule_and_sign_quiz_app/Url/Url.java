ddb8e5483e9b79ea5d7b8d367f8a0c2a3988d12b
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2020-01-16 17:16:43 +0545

    Base url for image upload is created

--- app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/Url/Url.java
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/Url/Url.java
@@ -7,6 +7,7 @@ public class Url{
 
     private static final String BASE_URL="http://10.0.2.2:5000/";
     public static String token="Bearer";
+    public static String imagePath = BASE_URL + "uploads/" ;
     public static Retrofit getInstance() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Url.BASE_URL)
