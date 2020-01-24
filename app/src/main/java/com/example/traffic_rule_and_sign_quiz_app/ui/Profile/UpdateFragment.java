c37665ac228ea282eb123c2ec62313076281f7f8
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2020-01-24 20:45:41 +0545

    Show profile done

--- app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/ui/Profile/UpdateFragment.java
+++ app/src/main/java/com/example/traffic_rule_and_sign_quiz_app/ui/Profile/UpdateFragment.java
@@ -1,21 +1,22 @@
 package com.example.traffic_rule_and_sign_quiz_app.ui.Profile;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.traffic_rule_and_sign_quiz_app.Methods.LoginRegister;
 import com.example.traffic_rule_and_sign_quiz_app.Methods.Strick;
@@ -28,15 +29,15 @@ import java.net.URL;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ProfileFragment extends Fragment {
+public class UpdateFragment extends Fragment {
     String id, token;
     SharedPreferences prfs;
     User_model model;
     LoginRegister loginRegister = new LoginRegister();
-    ImageView edit;
-    TextView firstname, lastname, dobtext, gendertext, emailtext, phonetext, username,fname;
-   CircleImageView imageView;
-   RelativeLayout relativeLayout;
+
+    EditText firstname, lastname, dobtext, gendertext, emailtext, phonetext, username, fname;
+    CircleImageView imageView;
+    RelativeLayout relativeLayout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,28 +50,20 @@ public class ProfileFragment extends Fragment {
         username = root.findViewById(R.id.username);
         emailtext = root.findViewById(R.id.email1);
         phonetext = root.findViewById(R.id.phone1);
-        fname=root.findViewById(R.id.fname);
-        edit=root.findViewById(R.id.edit);
+        fname = root.findViewById(R.id.fname);
+
         imageView = root.findViewById(R.id.post_profileimg);
-        relativeLayout=root.findViewById(R.id.layout1);
+        relativeLayout = root.findViewById(R.id.layout1);
 
-        edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                relativeLayout.setVisibility(View.GONE);
-                setFragment(new UpdateFragment());
-            }
-        });
 
         prfs = getContext().getSharedPreferences("session", Context.MODE_PRIVATE);
         id = prfs.getString("id", "");
         token = prfs.getString("token", "");
-     //   Toast.makeText(getContext(), token + id, Toast.LENGTH_SHORT).show();
+        //   Toast.makeText(getContext(), token + id, Toast.LENGTH_SHORT).show();
         model = loginRegister.userDetail(id, token);
         firstname.setText(model.getFirstname());
         username.setText(model.getUsername());
-        fname.setText(model.getFirstname()+" "+model.getLastname());
+        fname.setText(model.getFirstname() + " " + model.getLastname());
         gendertext.setText(model.getGender());
         emailtext.setText(model.getEmail());
         lastname.setText(model.getLastname());
@@ -89,15 +82,15 @@ public class ProfileFragment extends Fragment {
 
         return root;
     }
-    public boolean setFragment(Fragment fragment){
-        if(fragment != null)
-        {
-            getChildFragmentManager().beginTransaction().replace(R.id.layout,fragment).commit();
+
+    public boolean setFragment(Fragment fragment) {
+        if (fragment != null) {
+            getChildFragmentManager().beginTransaction().replace(R.id.layout, fragment).commit();
             return true;
         }
         return false;
     }
-
-
-
 }
+
+
+
