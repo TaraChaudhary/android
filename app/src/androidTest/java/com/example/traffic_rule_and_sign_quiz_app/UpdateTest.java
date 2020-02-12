f94de0b2b270b1a6bd6f3382310c67e3bb04a777
TaraChaudhary <56822814+TaraChaudhary@users.noreply.github.com>
2020-02-13 00:03:20 +0545

    Instrumental test is done success

--- app/src/androidTest/java/com/example/traffic_rule_and_sign_quiz_app/UpdateTest.java
+++ app/src/androidTest/java/com/example/traffic_rule_and_sign_quiz_app/UpdateTest.java
@@ -1,10 +1,10 @@
 package com.example.traffic_rule_and_sign_quiz_app;
 
-
 import androidx.test.rule.ActivityTestRule;
 
-import com.example.traffic_rule_and_sign_quiz_app.Methods.LoginRegister;
+import com.example.traffic_rule_and_sign_quiz_app.ui.Profile.UpdateFragment;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,24 +17,36 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
 @RunWith(JUnit4.class)
-public class InstrumentedTest {
-    @Rule
-    public ActivityTestRule<LoginActivity> rule =
-            new ActivityTestRule<>(LoginActivity.class);
-    String username = "admin";
-    String password = "tara";
+public class UpdateTest {
 
+    @Rule
+public ActivityTestRule<DashboardActivity> rule =
+        new ActivityTestRule<>(DashboardActivity.class);
+
+@Before
+public void Update()
+{
+    rule.getActivity().getFragmentManager().beginTransaction();
+
+
+}
 
     @Test
-    public void testApiLogin(){
-        onView(withId(R.id.username)).perform(typeText(username));
-        onView(withId(R.id.password)).perform(typeText(password));
+    public void testregister(){
+        onView(withId(R.id.username)).perform(typeText("as"));
+
+        onView(withId(R.id.first)).perform(typeText("as"));
+        onView(withId(R.id.last)).perform(typeText("as"));
+
+
+        onView(withId(R.id.email1)).perform(typeText("as"));
+        onView(withId(R.id.phone1)).perform(typeText("as"));
+        onView(withId(R.id.post_profileimg)).perform(typeText("as"));
+
         closeSoftKeyboard();
-        onView(withId(R.id.signin)).perform(click());
+        onView(withId(R.id.signup)).perform(click());
         onView(withId(R.id.dasboard))
                 .check(matches(isDisplayed()));
     }
 }
-
